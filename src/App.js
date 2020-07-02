22398c331e20933f8834e37a1640360f61e8bb59
Kenneth Dao <kennethdao08@gmail.com>
2020-07-02 14:08:29 -0700

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -11,12 +11,17 @@ class App extends Component {
       monsters : [], 
       searchField: ''
     }
+
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => this.setState({monsters: users}));
+  }
+
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value});
   }
 
   render() {
@@ -26,9 +31,10 @@ class App extends Component {
       )
     return(
       <div className="App">
+        <h1> Monsters Rolodex </h1>
         <SearchBox 
           placeholder = 'search monsters' 
-          handleChange = {e => this.setState({searchField: e.target.value})}
+          handleChange = {this.handleChange}
         />
         <CardList monsters= {filteredMonsters}>
           
