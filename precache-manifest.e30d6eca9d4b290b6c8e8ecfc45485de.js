72c1ac19464730d93c69b536e2a14fd9b8ad0624
Kenneth Dao <kennethdao08@gmail.com>
2020-07-02 14:07:47 -0700

    Updates

---
+++ precache-manifest.e30d6eca9d4b290b6c8e8ecfc45485de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46a81ee8cad6634a58a4c111025cd7c2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5924e5975a9dd3972390",
+    "url": "/monsters-rolodex/static/css/main.62922d8b.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5924e5975a9dd3972390",
+    "url": "/monsters-rolodex/static/js/main.7b605a01.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
